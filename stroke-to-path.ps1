b550a6bafee15d80e37a6a65631c75211452d7c2
Matt Gibbs <matt.gibbs@hpe.com>
2015-11-02 17:09:34 -0500

    Out-Null pipe to make ForEach Synchronous

--- stroke-to-path.ps1
+++ stroke-to-path.ps1
@@ -11,7 +11,7 @@ Copy-Item -Path $iconDirPath -Filter *.svg -Destination $outputDirectory
 $svgs = Get-ChildItem ($outputDirectory + "\*") -Filter *.svg
 
 ForEach($svg in $svgs) {
-	(Get-Content $svg).replace('<path ', '<path id="strokeToPath" ') | Set-Content $svg
+	(Get-Content $svg).replace('<path ', '<path id="strokeToPath" ') | Set-Content $svg | Out-Null
 }
 
 ForEach($svg in $svgs) {
