788f22fa1e36c6b362551db374c98d254b463dce
Matt Gibbs <matt.gibbs@hpe.com>
2015-11-02 16:16:47 -0500

    Tired of playing around with this....
    This is the final commit.  This idea is dead because Python will hold
    onto the file handle until it feels like garbage collecting it.  This
    means when you try to spawn the subprocess, inkscape can't open the file
    until the garbage collection occurs or the python.exe is totally dead.

--- stroke-to-path.py
+++ stroke-to-path.py
@@ -3,29 +3,32 @@ import sys
 import glob
 import os
 import ntpath
+import stat
+import time
 
 # C:\Users\matt\Desktop\test-icons\achievement.svg
 INKSCAPE_EXE_PATH = 'C:\\Program Files\\Inkscape\\inkscape.exe'
 GIVEN_PATH_ID = 'strokeToPath'
-OUTPUT_FOLDER_NAME = 'output'
 
 def main(args):
 
-    if len(args) < 1:
+    if len(args) < 2:
         print('No arguments found');
         return
 
-    cleanUp()
-    files_to_process = prepFiles(args[0])
+    cleanUp(args[1])
+    files_to_process = prepFiles(args[0], args[1])
+
     print(files_to_process)
+    time.sleep(10)
     processFiles(files_to_process)
 
 
-def prepFiles(icon_dir):
+def prepFiles(icon_dir, output_dir):
     files_to_process = []
     for svg_path in glob.glob('%s\\*.svg' % (icon_dir)):
         svg_filename = ntpath.basename(svg_path)
-        file_path_out = ntpath.join(os.path.dirname(os.path.realpath(__file__)), OUTPUT_FOLDER_NAME, svg_filename)
+        file_path_out = ntpath.join(output_dir, svg_filename)
         with open(svg_path, 'rt') as svg_file:
             with open(file_path_out, 'wt') as output_file:
                 for line in svg_file:
@@ -34,28 +37,29 @@ def prepFiles(icon_dir):
                         cut = i + len('path')
                         line = ''.join([line[0:cut], ' id="%s"' % (GIVEN_PATH_ID), line[cut:]])
                     output_file.write(line)
+                os.close(output_file.fileno())
         files_to_process.append(file_path_out)
     return files_to_process
 
 def processFiles(files_to_process):
     for path in files_to_process:
+        os.chmod(path, stat.S_IWRITE)
         cmd = [
             INKSCAPE_EXE_PATH,
             '--file="%s"' % (path),
-            '--verb="ToolNode"',
-            '--select="%s"' % (GIVEN_PATH_ID),
-            '--verb="StrokeToPath"',
-            '--verb="FileSave"',
-            '--verb="FileClose"'
+            #'--verb="ToolNode"',
+            #'--select="%s"' % (GIVEN_PATH_ID),
+            #'--verb="StrokeToPath"',
+            #'--verb="FileSave"',
+            #'--verb="FileClose"'
         ]
-
+        print(' '.join(cmd))
         subprocess.Popen(cmd)
 
-def cleanUp():
-    if not os.path.exists(OUTPUT_FOLDER_NAME):
-        os.makedirs(OUTPUT_FOLDER_NAME)
-    else:
-        for file in glob.glob('%s\\*.svg' % (OUTPUT_FOLDER_NAME)):
+def cleanUp(output_path):
+    if os.path.exists(output_path):
+        for file in glob.glob('%s\\*.svg' % (output_path)):
             os.remove(file)
+
 
 if __name__=='__main__':main(sys.argv[1:])
