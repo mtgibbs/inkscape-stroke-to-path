3a4a59f1e80f5bdad501168fa5dbdac524db147b
Matt Gibbs <matt.gibbs@hpe.com>
2015-11-03 11:02:16 -0500

    Prototyping the removal of the rectangle
    and learning some xml stuff in powershell!

---
+++ remove-bg-rectangle.ps1
@@ -0,0 +1,28 @@
+Param([string] $iconDirPath)
+$iconDirPath = $iconDirPath + "\*"
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDirectory = Split-Path $scriptPath
+$outputDirectory = $scriptDirectory + "\cleaned-output"
+
+New-Item -ItemType Directory -Force -Path $outputDirectory
+Remove-Item ($outputDirectory + "\*") -Include *.svg
+Copy-Item -Path $iconDirPath -Filter *.svg -Destination $outputDirectory
+
+$svgs = Get-ChildItem ($outputDirectory + "\*") -Filter *.svg
+
+ForEach($svg in $svgs) {
+
+	echo($svg)
+	[xml]$svgXml = Get-Content $svg
+
+	foreach ($rect in $svgXml.svg.g.rect) {
+		if($rect -ne $null -and $rect.height -eq 48 -and $rect.width -eq 48) {
+			echo "Found a rectangle.  Removing it."
+			$rect.ParentNode.RemoveChild($rect)
+		}
+	}
+
+	$svgXML.Save($svg)
+
+	#(Get-Content $svg).replace('<path ', '<path id="strokeToPath" ') | Set-Content $svg | Out-Null
+}
