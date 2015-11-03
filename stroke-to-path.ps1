f395504c0072441342d94fd72855c767f56b6fbd
Matt Gibbs <matt.gibbs@hpe.com>
2015-11-03 13:35:05 -0500

    Giving up on the xml namespace thing for now

--- stroke-to-path.ps1
+++ stroke-to-path.ps1
@@ -10,23 +10,9 @@ Copy-Item -Path $iconDirPath -Filter *.svg -Destination $outputDirectory
 
 $svgs = Get-ChildItem ($outputDirectory + "\*") -Filter *.svg
 
-$namespaces = New-Object System.Xml.XmlNamespaceManager
-$namespaces.AddNamespace("sketch", "http://lookslikeurl.com")
-
-$nameTable = New-Object System.Xml.NameTable
-$xmlReaderSettings = New-Object System.Xml.XmlReaderSettings $nameTable
-$xmlNameSpaceManager = New-Object System.Xml.XmlNamespaceManager $xmlReaderSettings.NameTable
-$xmlNameSpaceManager.AddNamespace("sketch", "http://fakeurl.com/2001/XMLSchema-instance")
-$xmlParserContext = New-Object System.Xml.XmlParserContext($null, $xmlNameSpaceManager, "", [System.Xml.XmlSpace]::Default)
-
-
 ForEach($svg in $svgs) {
 	echo($svg)
-
-	echo $svgXml
-	$xmlReader = [System.Xml.XmlReader]::Create($svg, $xmlReaderSettings, $xmlParserContext)
-	$svgXml = New-Object System.Xml.XmlDocument
-	$svgXml.Load($xmlReader)
+	[xml]$svgXml = Get-Content $svg
 
 	foreach ($rect in $svgXml.svg.g.rect) {
 		if ($rect.height -eq 48 -and $rect.width -eq 48) {
@@ -40,12 +26,12 @@ ForEach($svg in $svgs) {
 		if ($path.id -eq $null) {
 			$path.SetAttribute("id", "")
 		}
-
 		$path.id = ("strokeToPath" + $idCounter)
 	}
 
 	$svgXML.Save($svg)
+}
 
+ForEach($svg in $svgs) {
 	& 'C:\Program Files\Inkscape\inkscape.exe' --file="$svg" --verb="ToolNode" --select="strokeToPath0" --verb="StrokeToPath" --verb="FileSave" --verb="FileClose" --Close | Out-Null
-
 }
