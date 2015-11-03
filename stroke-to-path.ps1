e350f42b0f4012e0d7f7479ceaf0e7be7cf19b77
Matt Gibbs <matt.gibbs@hpe.com>
2015-11-03 14:07:26 -0500

    Build out the args to pass dynamically
    Just in case there are two strokes.

--- stroke-to-path.ps1
+++ stroke-to-path.ps1
@@ -10,10 +10,12 @@ Copy-Item -Path $iconDirPath -Filter *.svg -Destination $outputDirectory
 
 $svgs = Get-ChildItem ($outputDirectory + "\*") -Filter *.svg
 
+$cmdArgList = New-Object System.Collections.Generic.List[System.Object]
+
 ForEach($svg in $svgs) {
 	echo($svg)
 	[xml]$svgXml = Get-Content $svg
-
+	
 	foreach ($rect in $svgXml.svg.g.rect) {
 		if ($rect.height -eq 48 -and $rect.width -eq 48) {
 			echo "Found a rectangle.  Removing it."
@@ -21,17 +23,31 @@ ForEach($svg in $svgs) {
 		}
 	}
 
+	$cmdArgs = New-Object System.Collections.Generic.List[String]
+	$cmdArgs.Add('--file="' + $svg +'"')
+	$cmdArgs.Add('--verb="ToolNode"')
+
 	$idCounter = 0
 	foreach ($path in $svgXml.svg.g.path) {
 		if ($path.id -eq $null) {
 			$path.SetAttribute("id", "")
 		}
 		$path.id = ("strokeToPath" + $idCounter)
+		$cmdArgs.Add('--select="strokeToPath' + $idCounter +'"')
+		$cmdArgs.Add('--verb="StrokeToPath"')
+		$idCounter++
 	}
 
+	$cmdArgs.Add('--verb="FileSave"')
+	$cmdArgs.Add('--verb="FileClose"')
+
 	$svgXML.Save($svg)
+
+	$cmdArgList.Add($cmdArgs)
 }
 
-ForEach($svg in $svgs) {
-	& 'C:\Program Files\Inkscape\inkscape.exe' --file="$svg" --verb="ToolNode" --select="strokeToPath0" --verb="StrokeToPath" --verb="FileSave" --verb="FileClose" --Close | Out-Null
+ForEach($cmdArg in $cmdArgList) {
+	echo "Executing inkscape: "
+	echo $cmdArg
+	& 'C:\Program Files\Inkscape\inkscape.exe' $cmdArg | Out-Null
 }
