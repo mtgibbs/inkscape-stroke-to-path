0a194c59bb2b9ed627bce4f7deb1731ed010f220
Matt Gibbs <matt.gibbs@hpe.com>
2015-11-03 12:13:05 -0500

    Trying to fix the prefix issue with some SVGs

--- stroke-to-path.ps1
+++ stroke-to-path.ps1
@@ -10,11 +10,42 @@ Copy-Item -Path $iconDirPath -Filter *.svg -Destination $outputDirectory
 
 $svgs = Get-ChildItem ($outputDirectory + "\*") -Filter *.svg
 
+$namespaces = New-Object System.Xml.XmlNamespaceManager
+$namespaces.AddNamespace("sketch", "http://lookslikeurl.com")
+
+$nameTable = New-Object System.Xml.NameTable
+$xmlReaderSettings = New-Object System.Xml.XmlReaderSettings $nameTable
+$xmlNameSpaceManager = New-Object System.Xml.XmlNamespaceManager $xmlReaderSettings.NameTable
+$xmlNameSpaceManager.AddNamespace("sketch", "http://fakeurl.com/2001/XMLSchema-instance")
+$xmlParserContext = New-Object System.Xml.XmlParserContext($null, $xmlNameSpaceManager, "", [System.Xml.XmlSpace]::Default)
+
 
 ForEach($svg in $svgs) {
-	(Get-Content $svg).replace('<path ', '<path id="strokeToPath" ') | Set-Content $svg | Out-Null
-}
+	echo($svg)
 
-ForEach($svg in $svgs) {
-	& 'C:\Program Files\Inkscape\inkscape.exe' --file="$svg" --verb="ToolNode" --select="strokeToPath" --verb="StrokeToPath" --verb="FileSave" --verb="FileClose" --verb="Close" | Out-Null
+	echo $svgXml
+	$xmlReader = [System.Xml.XmlReader]::Create($svg, $xmlReaderSettings, $xmlParserContext)
+	$svgXml = New-Object System.Xml.XmlDocument
+	$svgXml.Load($xmlReader)
+
+	foreach ($rect in $svgXml.svg.g.rect) {
+		if ($rect.height -eq 48 -and $rect.width -eq 48) {
+			echo "Found a rectangle.  Removing it."
+			$rect.ParentNode.RemoveChild($rect)
+		}
+	}
+
+	$idCounter = 0
+	foreach ($path in $svgXml.svg.g.path) {
+		if ($path.id -eq $null) {
+			$path.SetAttribute("id", "")
+		}
+
+		$path.id = ("strokeToPath" + $idCounter)
+	}
+
+	$svgXML.Save($svg)
+
+	& 'C:\Program Files\Inkscape\inkscape.exe' --file="$svg" --verb="ToolNode" --select="strokeToPath0" --verb="StrokeToPath" --verb="FileSave" --verb="FileClose" --Close | Out-Null
+
 }
